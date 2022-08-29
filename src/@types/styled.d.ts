1d83f5ae625953d358dad6fc2158e26b6bfb15d4
João <joaopedronk22@gmail.com>
2022-08-28 23:03:39 -0300

    Tipagem de temas

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default';
+
+// Tipando o tipo do conteudo que tem escrito no tema padrão
+type ThemeType = typeof defaultTheme;
+declare module 'styled-components'{
+    export interface DefaultTheme extends ThemeType {}
+}
