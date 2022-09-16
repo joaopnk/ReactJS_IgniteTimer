f513e589a500c26b2ead46a319f978ff8f8b600b
João <joaopedronk22@gmail.com>
2022-09-15 21:49:02 -0300

    Iniciando novo ciclo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,6 +11,7 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
 
 // Obj para regras de validação
 const newCycleFormValidationScheme = zod.object({
@@ -21,7 +22,16 @@ const newCycleFormValidationScheme = zod.object({
 // Criando tipagem a partir da referencia do ZOD (newCycleFormValidationScheme) | usando type of pra referencia o javascript dentro do typescript
 type NewCyecleFormData = zod.infer<typeof newCycleFormValidationScheme>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCyecleFormData>({
     resolver: zodResolver(newCycleFormValidationScheme),
     // VAlores iniciais de cada campo
@@ -32,10 +42,27 @@ export function Home() {
   })
 
   function handleCreateNewCycle(data: NewCyecleFormData) {
-    console.log(data)
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle])
+
+    // Setando o ciclo recem criado como ciclo ativo
+    setActiveCycleId(id)
+
     // Voltando os campos (resetando) para os valores originais (baseados no defaultValues!)
     reset()
   }
+
+  // Percorrendo o ciclos procurando o ciclo que esta ativo
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  console.log(activeCycle)
 
   // Observando o campo de task para habilitar/desabilitar o botão de começar!
   const task = watch('task')
